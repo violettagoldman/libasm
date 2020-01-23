1f2bed8ad7ae85793901b283c6c7ae8e7dd94efa
violettagoldman <violagoldman@mail.ru>
2020-01-23 11:18:52 +0100

    main cont

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "libftprintf.h"
+#include <s
 
 int		ft_strlen(const char *s);
 char	*ft_stpcpy(char *dst, const char * src);
@@ -7,24 +8,57 @@ int		ft_write(int fildes, const void *buf, size_t nbyte);
 int		ft_read(int fildes, void *buf, size_t nbyte);
 char	*ft_strdup(const char *s1);
 
-int		ft_assert(int a, int b)
+void	ft_assert(int a, int b)
 {
 	if (a == b)
-		printf("\033");
+		ft_printf("\033[1;32m[OK]\033[0m");
 	else
-		printf();
+		ft_printf("\033[1;31m[FAIL]\033[0m");
 }
+
+/*
+ * DISCLAIMER!
+ * ALL functions are compared to the ouptput of original functions.
+ */
 
 int		main(void)
 {
-	ft_printf("\033[1;33mft_strlen\n");
+	ft_printf("\033[0;33mft_strlen ");
+	//simple test
+	ft_assert(ft_strlen("test"), 4);
+	//very long test
+	ft_assert(ft_strlen("Lorem ipsum dolor sit amet, consectetur adipiscing\
+	elit. Integer accumsan nec urna sit amet efficitur. Sed sit amet pulvinar\
+	erat, eu interdum velit. Aliquam erat volutpat. Integer nec velit purus.\
+	Cras cursus, nibh vel consequat imperdiet, nisl urna venenatis est, nec\
+	elementum lacus ligula vitae lectus. Nam massa urna, placerat a mauris\
+	eget, gravida volutpat dui. Sed faucibus felis et risus rhoncus, sit amet\
+	suscipit magna varius. Donec at turpis mauris. Sed tincidunt, nisl vitae\
+	euismod eleifend, magna urna viverra sapien, eget lacinia tortor velit non\
+	sapien. Nulla orci risus, eleifend eu interdum at, malesuada in lacus.\
+	Aenean consequat facilisis diam a porttitor."), 678);
+	//Empty string test
+	ft_assert(ft_strlen(""), 0);
+	//'\0' test
+	ft_assert(ft_strlen("\0FAIL"), 0);
+	ft_printf("\n\033[0;33mft_strcpy\033[0m ");
+	ft_assert(ft_strcpy("Lorem ipsum dolor sit amet, consectetur adipiscing\
+	elit. Integer accumsan nec urna sit amet efficitur. Sed sit amet pulvinar\
+	erat, eu interdum velit. Aliquam erat volutpat. Integer nec velit purus. \
+	Cras cursus, nibh vel consequat imperdiet, nisl urna venenatis est, nec\
+	elementum lacus ligula vitae lectus. Nam massa urna, placerat a mauris\
+	eget, gravida volutpat dui. Sed faucibus felis et risus rhoncus, sit amet\
+	suscipit magna varius. Donec at turpis mauris. Sed\ tincidunt, nisl vitae\
+	euismod eleifend, magna urna viverra sapien, eget lacinia tortor velit non \
+	sapien. Nulla orci risus, eleifend eu interdum at, malesuada in lacus. \
+	Aenean consequat facilisis diam a porttitor."));
+
+	ft_printf("\n\033[0;33mft_strdup\033[0m");
+	ft_assert
+	free()
+	= NULL;
+	ft_printf("\n\033[0;33mft_write\033[0m");
 	
-	ft_printf("\033[1;33mft_strcpy\n");
-
-	ft_printf("\033[1;33mft_strdup\n");
-
-	ft_printf("\033[1;33mft_write\n");
-
-	ft_printf("\033[1;33mft_read\n");
+	ft_printf("\n\033[0;33mft_read\033[0m");
 	return (0);
 }
