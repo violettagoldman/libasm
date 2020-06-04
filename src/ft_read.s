0ead06710e4b4be1b7905543657b95ec64e148eb
violettagoldman <violagoldman@mail.ru>
2020-06-04 21:18:40 +0200

    ft_read: ft_write: errno added

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,6 +1,15 @@
 global ft_read
+extern __errno_location
 
 ft_read:
 	mov rax, 0
 	syscall
+	jc _error
+	ret
+
+_error:
+	mov r8, rax
+	call __errno_location
+	mov [rax], r8
+	mov rax, -1 
 	ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,6 +1,16 @@
 global ft_write
+extern __errno_location
 
 ft_write:
 	mov rax, 1
 	syscall
+	jc _error
 	ret
+
+_error:
+	mov r8, rax
+	call __errno_location
+	mov [rax], r8
+	mov rax, -1 
+	ret
+
